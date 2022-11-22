9991c7291792e196d8511e0d51ba5bdf768e8d96
Administrator <Administrator@User-2022YIVWQM>
2022-11-22 11:54:00 +0900

    업뎃

---
+++ JungOl/src/string/q601/Main.java
@@ -0,0 +1,26 @@
+package string.q601;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		sc.close();
+		char[] ch = str.toCharArray();
+		for (int i = 1; i <= str.length(); i++) {
+			for (int j = str.length() - i; j < str.length(); j++) {
+				System.out.print(ch[j]);
+			}
+			for (int j = 0; j < str.length() - i; j++) {
+				System.out.print(ch[j]);
+			}
+
+			System.out.println();
+		}
+		for (int i = 0; i < str.length(); i++) {
+
+		}
+	}
+
+}
