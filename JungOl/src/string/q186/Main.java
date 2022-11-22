6b8652b6ee658541cb93c20a96710320f8ad713c
Administrator <Administrator@User-2022YIVWQM>
2022-11-22 12:48:33 +0900

    문자여 186

---
+++ JungOl/src/string/q186/Main.java
@@ -0,0 +1,25 @@
+package string.q186;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String[] str = new String[2];
+		int max = Integer.MIN_VALUE;
+		for (int i = 0; i < str.length; i++) {
+			str[i] = sc.next();
+		}
+
+		for (int i = 0; i < str.length; i++) {
+			if (str[i].length() > max) {
+				max = str[i].length();
+			}
+		}
+
+		System.out.println(max);
+
+	}
+
+}
