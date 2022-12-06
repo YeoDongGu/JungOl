ee3aac5bf30179143238fa797a9460f1665a0e13
Administrator <Administrator@User-2022YIVWQM>
2022-12-06 16:46:15 +0900

    129번 문제풀이

--- JungOl/src/q129/Main.java
+++ JungOl/src/q129/Main.java
@@ -6,24 +6,21 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int base = 1;
-		int height = 1;
-		char ch;
+
 		while (true) {
-			base = sc.nextInt();
-			height = sc.nextInt();
 			System.out.printf("Base = ");
+			int base = sc.nextInt();
 			System.out.printf("Height = ");
+			int height = sc.nextInt();
 			System.out.printf("Triangle width = %.1f\n", (double) height * (double) base / 2);
 			System.out.printf("Continue? ");
 
-			ch = sc.next().charAt(0);
+			char ch = sc.next().charAt(0);
 
-			if (ch == 'y' || ch == 'Y') {
-
-			} else {
+			if (ch != 'y' && ch != 'Y') {
 				break;
 			}
+
 		}
 		sc.close();
 	}
