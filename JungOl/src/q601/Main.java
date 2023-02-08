1bd0d7c46597c99dc6c0d6102addc5010d317236
Administrator <nanovia3@naver.com>
2023-02-08 09:47:45 +0900

    문제풀이 업뎃

--- JungOl/src/q601/Main.java
+++ JungOl/src/q601/Main.java
@@ -3,12 +3,21 @@ package q601;
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		
+		String str = sc.next();
+		sc.close();
+		char[] ch = str.toCharArray();
+		for (int i = 1; i <= str.length(); i++) {
+			for (int j = str.length() - i; j < str.length(); j++) {
+				System.out.print(ch[j]);
+			}
+			for (int j = 0; j < str.length() - i; j++) {
+				System.out.print(ch[j]);
+			}
 
+			System.out.println();
+		}
 	}
 
 }
---
+++ JungOl/src/q601/Main2.java
@@ -0,0 +1,11 @@
+package q601;
+
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String inp = sc.next();
+
+    }
+}
